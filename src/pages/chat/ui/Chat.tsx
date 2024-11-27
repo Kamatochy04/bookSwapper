a05da9ddd6984a4cc3e445d2f22d63f73263fd7e
Andrey <andreii.rubik@mail.ru>
2024-11-27 10:13:51 +0300

    fix css

--- src/pages/chat/ui/Chat.tsx
+++ src/pages/chat/ui/Chat.tsx
@@ -1,13 +1,21 @@
+import { LeftOutlined, SendOutlined } from "@ant-design/icons";
 import styles from "./chat.module.scss";
+import { useNavigate } from "react-router-dom";
 
 const Chat = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.chat}>
-      {/* <Container> */}
       <div className={styles.container}>
         <div className={styles.header}>
-          <div className={styles.avatart}>А</div>
-          <p className={styles.name}>Андрей Валюк</p>
+          <div className={styles.arrow} onClick={() => navigate(-1)}>
+            <LeftOutlined />
+          </div>
+          <div className={styles.user}>
+            <div className={styles.avatart}>А</div>
+            <p className={styles.name}>Андрей Валюк</p>
+          </div>
         </div>
         <div className={styles.messages}></div>
         <div className={styles.footer}>
@@ -16,6 +24,9 @@ const Chat = () => {
             placeholder="Сообщение"
             className={styles.footer__input}
           />
+          <div className={styles.send}>
+            <SendOutlined />
+          </div>
         </div>
       </div>
     </div>
