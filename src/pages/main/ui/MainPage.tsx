0db2e52e5fe4801deaf3d8c7098e3b86159c4f42
Andrey Valiuk <valyuk.andrey.04@mail.ru>
2024-12-15 21:56:10 +0300

    commit

--- src/pages/main/ui/MainPage.tsx
+++ src/pages/main/ui/MainPage.tsx
@@ -1,33 +1,67 @@
-import { Container } from "../../../shared/components";
-import { BookPrevieCard } from "../../../widgets";
+import { Container, Input } from "../../../shared/components";
+// import { BookPrevieCard } from "../../../widgets";
 
-import img from "../../../shared/assets/book_1.jpg";
-import img_2 from "../../../shared/assets/book_2.jpg";
-import img_3 from "../../../shared/assets/book_3.jpg";
+// import img from "../../../shared/assets/book_1.jpg";
+// import img_2 from "../../../shared/assets/book_2.jpg";
+// import img_3 from "../../../shared/assets/book_3.jpg";
 
 import styles from "./mainPage.module.scss";
+import { CustomerServiceOutlined, SearchOutlined } from "@ant-design/icons";
 
-const books = [
-  { name: "Скажи жизни да", author: "Андрей Валюк", img: img, id: 1 },
-  { name: "Сто лет одиночества", author: "Андрей Валюк", img: img_2, id: 2 },
-  { name: "Мы", author: "Андрей Валюк", img: img_3, id: 3 },
-  // { name: "Скажи жизни да", author: "Андрей Валюк", img: img, id: 4 },
-  // { name: "Сто лет одиночества", author: "Андрей Валюк", img: img_2, id: 5 },
-  // { name: "Мы", author: "Андрей Валюк", img: img_3, id: 6 },
-  // { name: "Скажи жизни да", author: "Андрей Валюк", img: img, id: 7 },
-  // { name: "Сто лет одиночества", author: "Андрей Валюк", img: img_2, id: 8 },
-  // { name: "Мы", author: "Андрей Валюк", img: img_3, id: 9 },
-];
+// const books = [
+//   { name: "Скажи жизни да", author: "Андрей Валюк", img: img, id: 1 },
+//   { name: "Сто лет одиночества", author: "Андрей Валюк", img: img_2, id: 2 },
+//   { name: "Мы", author: "Андрей Валюк", img: img_3, id: 3 },
+// ];
 
 const MainPage = () => {
   return (
     <>
       <div className={styles.books}>
         <Container>
-          <div className={styles.books__container}>
-            {books.map((item) => {
-              return <BookPrevieCard {...item} />;
-            })}
+          <div className={styles.input}>
+            <Input placeholder="Поиск книги ..."/>
+            <div className={styles.icon}>
+              <SearchOutlined />
+            </div>
+          </div>
+
+          <div className={styles.genre}>
+            <h3 className={styles.genre__title}>Жанры</h3>
+            <div className={styles.genre__container}>
+              <div className={styles.genre__icon}>
+                <CustomerServiceOutlined />
+                <p className={styles.genre__icon_text}>Жанр</p>
+              </div>
+              <div className={styles.genre__icon}>
+                <CustomerServiceOutlined />
+                <p className={styles.genre__icon_text}>Жанр</p>
+              </div>
+              <div className={styles.genre__icon}>
+                <CustomerServiceOutlined />
+                <p className={styles.genre__icon_text}>Жанр</p>
+              </div>
+              <div className={styles.genre__icon}>
+                <CustomerServiceOutlined />
+                <p className={styles.genre__icon_text}>Жанр</p>
+              </div>
+              <div className={styles.genre__icon}>
+                <CustomerServiceOutlined />
+                <p className={styles.genre__icon_text}>Жанр</p>
+              </div>
+              <div className={styles.genre__icon}>
+                <CustomerServiceOutlined />
+                <p className={styles.genre__icon_text}>Жанр</p>
+              </div>
+              <div className={styles.genre__icon}>
+                <CustomerServiceOutlined />
+                <p className={styles.genre__icon_text}>Жанр</p>
+              </div>
+            </div>
+          </div>
+        
+          <div className={styles.list}>
+            asd
           </div>
         </Container>
       </div>
