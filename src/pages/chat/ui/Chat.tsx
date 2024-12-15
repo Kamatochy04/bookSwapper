b75bd4d40bf779ca711c601603ceec9bed5219e0
Andrey Valiuk <valyuk.andrey.04@mail.ru>
2024-12-15 22:57:24 +0300

    sdf:

--- src/pages/chat/ui/Chat.tsx
+++ src/pages/chat/ui/Chat.tsx
@@ -1,9 +1,24 @@
 import { LeftOutlined, SendOutlined } from "@ant-design/icons";
 import styles from "./chat.module.scss";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+type Message = { 
+  id: number; 
+  text: string; 
+  sender: string;
+}
 
 const Chat = () => {
+  const [messages, setMessage] = useState<Message[]>([])
+  const [text, setText] = useState("")
   const navigate = useNavigate();
+
+  const handleSendMessage = (sender: string) => { 
+    const newMessage: Message = { id: messages.length + 1, text, sender, }; 
+    setMessage([...messages, newMessage]); 
+    setText("")
+  };
 
   return (
     <div className={styles.chat}>
@@ -17,14 +32,18 @@ const Chat = () => {
             <p className={styles.name}>Андрей Валюк</p>
           </div>
         </div>
-        <div className={styles.messages}></div>
+        <div className={styles.messages}>
+          {messages.map(item => (<p className={styles.message__item}>{item.text}</p>))}
+        </div>
         <div className={styles.footer}>
           <input
             type="text"
             placeholder="Сообщение"
             className={styles.footer__input}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
           />
-          <div className={styles.send}>
+          <div className={styles.send} onClick={() => handleSendMessage("sdfsdfsd")}>
             <SendOutlined />
           </div>
         </div>
